6507e8f1e2366ff0dc4283b805b792304562acfb
Christian Strickland <101130958+cstrickland-WE@users.noreply.github.com>
2022-07-08 20:37:42 -0500

    Initial commit

---
+++ src/main/java/com/example/pokedex/PokedexMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.pokedex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedexMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokedexMicroserviceApplication.class, args);
+	}
+
+}
